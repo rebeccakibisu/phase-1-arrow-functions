04d9a001f50611c46780bcefdefb9e6a29ff0121
Rebecca Kibisu <coursecpd@gmail.com>
2025-06-16 07:01:57 +0300

    func

---
+++ index.js
@@ -0,0 +1,10 @@
+// Function expression for divide
+const divide = function(a, b) {
+  return a / b;
+};
+
+// Arrow function for square
+const square = (x) => x * x;
+
+// Arrow function for add
+const add = (a, b) => a + b;
